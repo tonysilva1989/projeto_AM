a5203d84af6e0f73eb02e477181b044a170f5335
phmb2 <phmb2@cin.ufpe.br>
2016-10-14 08:16:18 -0300

    Adicionar arquivo ler_dados

---
+++ ler_dados.m
@@ -0,0 +1,58 @@
+
+function [X, N, p, K, particaoClasses, classesPadroes] = ler_dados()
+
+    fid = fopen('C:\Pedro\Projeto AM\abalone.data');
+
+    file = textscan(fid, '%s%s%s%s%s%s%s%s%s', 'delimitador', ',');
+
+    H = horzcat(file{:});
+
+    A = H(:, 2:end);   
+    %TESTE
+    %A = H(1:50, 2:end); 
+    
+    N = size(A, 1);
+
+    p = size(A, 2);
+
+    X = convertToDouble(A, N, p);
+
+    X = normalizar5(X, p);
+
+    labels = ['M', 'F', 'I'];
+
+    K = length(labels);
+   
+    classes = H(:, 1);
+    
+    classes = cell2mat(classes);
+    
+    [particaoClasses, classesPadroes] = criarParticaoClasses(classes, N, K, labels);
+end
+
+function X = convertToDouble(A, N, p)
+    X = zeros(N, p);
+    
+    for i = 1 : N 
+        for j = 1 : p
+            X(i, j) = str2double(A(i, j));
+        end
+    end
+end
+
+function X = normalizar(X, p)
+    for j = 1 : p
+        X(:, j) = (X(:, j) - min(X(:, j))) ./ (max(X(:, j) - min(X(:, j))));
+    end
+end
+
+function [particaoClasses, classesPadroes] = criarParticaoClasses(classes, N, K, labels) 
+    particaoClasses = cell(1, K);
+    classesPadroes = zeros(1, N);
+    
+    for i = 1 : N
+       classe = find(labels == classes(i));        
+       particaoClasses{1, classe} = [particaoClasses{1, classe} i];	
+       classesPadroes(i) = classe;  
+    end
+end
